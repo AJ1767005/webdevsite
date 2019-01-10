33251349b756f8d7671f4b0164464aae5fd3affc
AJ1767005 <46560474+AJ1767005@users.noreply.github.com>
2019-01-10 14:57:07 +0000

    Create music.js

---
+++ JS/music.js
@@ -0,0 +1,20 @@
+var mute1 = 0;
+function Mute(){
+if(mute1 == 0)
+{
+    document.getElementById("background").volume = 0.0;
+    mute1 = 1;
+    document.getElementById("Mute").innerHTML ="Unmute Sound";
+
+
+
+}
+
+else{
+document.getElementById("background").volume="0.05"    ;
+mute1 = 0;
+document.getElementById("Mute").innerHTML = "Mute Sound";
+
+}
+
+}
